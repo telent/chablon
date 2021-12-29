2d1dde850d9fd27bd11798946582b63bc4109996
Daniel Barlow <dan@telent.net>
2021-12-29 12:29:57 +0000

    allow unencrypted writes

--- ble.c
+++ ble.c
@@ -305,7 +305,7 @@ static const ble_uuid128_t gatt_svr_chr_sec_test_static_uuid =
         BLE_UUID128_INIT(0xf7, 0x6d, 0xc9, 0x07, 0x71, 0x00, 0x16, 0xb0,
                          0xe1, 0x45, 0x7e, 0x89, 0x9e, 0x65, 0x3a, 0x5c);
 
-static uint8_t gatt_svr_sec_test_static_val;
+static uint8_t gatt_svr_sec_test_static_val = 42;
 
 static int
 gatt_svr_chr_access_sec_test(uint16_t conn_handle, uint16_t attr_handle,
@@ -321,13 +321,18 @@ static const struct ble_gatt_svc_def gatt_svr_svcs[] = {
             /*** Characteristic: Random number generator. */
             .uuid = &gatt_svr_chr_sec_test_rand_uuid.u,
             .access_cb = gatt_svr_chr_access_sec_test,
-            .flags = BLE_GATT_CHR_F_READ | BLE_GATT_CHR_F_READ_ENC,
+	    // if I include READ_ENC here then any attempt
+	    // to read is rejected, even if READ and READ_ENC
+	    // are both present
+            .flags = BLE_GATT_CHR_F_READ,// | BLE_GATT_CHR_F_READ_ENC,
         }, {
             /*** Characteristic: Static value. */
             .uuid = &gatt_svr_chr_sec_test_static_uuid.u,
             .access_cb = gatt_svr_chr_access_sec_test,
+	    // here similarly, including WRITE_ENC causes
+	    // plaintext writes to be rejected
             .flags = BLE_GATT_CHR_F_READ |
-                     BLE_GATT_CHR_F_WRITE | BLE_GATT_CHR_F_WRITE_ENC,
+	    BLE_GATT_CHR_F_WRITE,// | BLE_GATT_CHR_F_WRITE_ENC,
         }, {
             0, /* No more characteristics in this service. */
         } },
